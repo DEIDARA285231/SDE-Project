fb490b47a242b5a2a6c26718d8abdd90f474467f
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2021-01-16 12:03:04 +0100

    aggiunte le chiavi di Is there any deal

--- secrets.ts
+++ secrets.ts
@@ -1,5 +1,7 @@
 export default {
-  MAPQUEST_KEY: 'your-api-key',
   CLIENT_SECRET: "qdjrkcxp4q5jtqzjm1qtk46ejk20gz",
-  CLIENT_ID: "eizkab37usgvovmohkoug9x2toeg2x"
+  CLIENT_ID: "eizkab37usgvovmohkoug9x2toeg2x",
+  ITAD_ID: "997250fdd81e7711",
+  ITAD_SECRET: "174fe0520f548ee743f171a02f84ddb41fbabe7f",
+  ITAD_KEY: "ce70aa8d8dfd19342b24c7cc999c8f96d1bdda87"
 }
