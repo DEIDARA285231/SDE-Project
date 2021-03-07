cb02383f48508eba20e4484005cc8e847c5fdfbd
Extrance <balasso.andrea69@gmail.com>
2021-03-07 10:28:19 +0100

    updated frontend

--- routes/index.js
+++ routes/index.js
@@ -181,13 +181,18 @@ router.get('/game', ensureAuth, async (req,res) => {
       } else {
         url = "https://static-cdn.jtvnw.net/ttv-static/404_boxart.jpg"
       }
+      //aggregated_rating check and format
+      if(typeof game.data.aggregated_rating === 'undefined') {
+        game.data.aggregated_rating = 'N/A'
+      } else {
+        let rat = ''+game.data.aggregated_rating+'';
+        game.data.aggregated_rating = rat.substring(0,4);
+      }
       //rating check and format
-      var rat2 = ''+game.data.aggregated_rating+'';
-      game.data.aggregated_rating = rat2.substring(0,4);
       if(typeof game.data.rating === 'undefined') {
         game.data.rating = 'N/A'
       } else {
-        var rat = ''+game.data.rating+'';
+        let rat = ''+game.data.rating+'';
         game.data.rating = rat.substring(0,4);
       }
       //date format
@@ -196,66 +201,38 @@ router.get('/game', ensureAuth, async (req,res) => {
         game.data.first_release_date = dat.substring(5,16)
       }
 
-      var price = ""
-
-      var priceAmazon = "N/A"
-      var hoursAmazon = "N/A"
-      var priceSteam = "N/A"
-      var hoursSteam = "N/A"
-      var priceGog = "N/A"
-      var hoursGog = "N/A"
-      var priceOrigin = "N/A"
-      var hoursOrigin = "N/A"
-      var priceEpic = "N/A"
-      var hoursEpic = "N/A"
+      var pricesArray = [];
 
       if(prices !== undefined) {
-        console.log(prices.data)
-        /*for(let i=0; i<prices.data.stores.length; i++) {
-          console.log(prices.data.stores[i])
-          switch(prices.data.stores[i].storeName){
+        for(let i=0; i<prices.data.stores.length; i++) {
+          switch(prices.data.stores[i].storeName) {
             case "amazonus":
-              priceAmazon = prices.data.stores[i].lowestPrice;
-              hoursAmazon = prices.data.stores[i].hoursPerEuroRatio;
+              prices.data.stores[i].storeName = "Amazon";
               break;
             case "steam":
-              priceSteam = prices.data.stores[i].lowestPrice;
-              hoursSteam = prices.data.stores[i].hoursPerEuroRatio;
+              prices.data.stores[i].storeName = "Steam";
               break;
-            case "amazonus":
-              priceAmazon = prices.data.stores[i].lowestPrice;
-              hoursAmazon = prices.data.stores[i].hoursPerEuroRatio;
+            case "origin":
+              prices.data.stores[i].storeName = "Origin";
+              break;
+            case "epic":
+              prices.data.stores[i].storeName = "Epic Games";
+              break;
+            case "gog":
+              prices.data.stores[i].storeName = "GOG.com";
+              break;
+            default:
               break;
           }
-        }*/
+          pricesArray.push(prices.data.stores[i])
+        }
       }
-
-
 
       //render views/game with these params
       res.render('game', {
         data: game.data,
-        cover: url
-        /*priceAmazon: {
-          lowestPrice: prices.data.stores[0].lowestPrice,
-          hoursPerEuroRatio: prices.data.stores[0].hoursPerEuroRatio
-        },
-        priceSteam: {
-          lowestPrice: prices.data.stores[0].lowestPrice,
-          hoursPerEuroRatio: prices.data.stores[0].hoursPerEuroRatio
-        },
-        priceGog: {
-          lowestPrice: prices.data.stores[0].lowestPrice,
-          hoursPerEuroRatio: prices.data.stores[0].hoursPerEuroRatio
-        },
-        priceOrigin: {
-          lowestPrice: prices.data.stores[0].lowestPrice,
-          hoursPerEuroRatio: prices.data.stores[0].hoursPerEuroRatio
-        },
-        priceEpic: {
-          lowestPrice: prices.data.stores[0].lowestPrice,
-          hoursPerEuroRatio: prices.data.stores[0].hoursPerEuroRatio
-        }*/
+        cover: url,
+        prices: pricesArray
       });
 
     } else if(name !== false) {
@@ -267,7 +244,7 @@ router.get('/game', ensureAuth, async (req,res) => {
           'Accept': 'application/json',
         },
       });
-
+      //get the results on Twitch given the name
       const twitchSearch = await axios({
         url: `http://localhost:3000/api/twitch/search?query=${name}`,
         method: 'GET',
@@ -275,67 +252,31 @@ router.get('/game', ensureAuth, async (req,res) => {
           'Accept': 'application/json',
         },
       });
+      //arrays of items we'll pass to handlebars
+      var search = [];
+      var twitch = [];
 
-      for(var i=0; i<game.data.length; i++) {
+      //we format and then push the items in the array we use on the frontend
+      for(let i=0; i<game.data.length; i++) {
         var dat = ''+game.data[i].first_release_date+'';
         if(dat != "Invalid Date") {
           game.data[i].first_release_date = dat.substring(5,16)
         }
+        search.push(game.data[i])
       }
-
-      for(var i=0; i<twitchSearch.data.data.length; i++) {
+      //we format and then push the items in the array we use on the frontend
+      for(let i=0; i<twitchSearch.data.data.length; i++) {
         var img = twitchSearch.data.data[i].box_art_url;
         img = img.split("52x72.jpg")
         twitchSearch.data.data[i].box_art_url = img[0]+"520x720.jpg"
+        twitch.push(twitchSearch.data.data[i])
       }
 
       res.render('gameList', {
         query: name,
         length: game.data.length,
-        search: {
-          entry0: 0<game.data.length ? game.data[0] : '',
-          entry1: 1<game.data.length ? game.data[1] : '',
-          entry2: 2<game.data.length ? game.data[2] : '',
-          entry3: 3<game.data.length ? game.data[3] : '',
-          entry4: 4<game.data.length ? game.data[4] : '',
-          entry5: 5<game.data.length ? game.data[5] : '',
-          entry6: 6<game.data.length ? game.data[6] : '',
-          entry7: 7<game.data.length ? game.data[7] : '',
-          entry8: 8<game.data.length ? game.data[8] : '',
-          entry9: 9<game.data.length ? game.data[9] : '',
-          entry10: 10<game.data.length ? game.data[10] : '',
-          entry11: 11<game.data.length ? game.data[11] : '',
-          entry12: 12<game.data.length ? game.data[12] : '',
-          entry13: 13<game.data.length ? game.data[13] : '',
-          entry14: 14<game.data.length ? game.data[14] : '',
-          entry15: 15<game.data.length ? game.data[15] : '',
-          entry16: 16<game.data.length ? game.data[16] : '',
-          entry17: 17<game.data.length ? game.data[17] : '',
-          entry18: 18<game.data.length ? game.data[18] : '',
-          entry19: 19<game.data.length ? game.data[19] : '',
-        },
-        twitch: {
-          entry0: 0<twitchSearch.data.data.length ? twitchSearch.data.data[0] : undefined,
-          entry1: 1<twitchSearch.data.data.length ? twitchSearch.data.data[1] : '',
-          entry2: 2<twitchSearch.data.data.length ? twitchSearch.data.data[2] : '',
-          entry3: 3<twitchSearch.data.data.length ? twitchSearch.data.data[3] : '',
-          entry4: 4<twitchSearch.data.data.length ? twitchSearch.data.data[4] : '',
-          entry5: 5<twitchSearch.data.data.length ? twitchSearch.data.data[5] : '',
-          entry6: 6<twitchSearch.data.data.length ? twitchSearch.data.data[6] : '',
-          entry7: 7<twitchSearch.data.data.length ? twitchSearch.data.data[7] : '',
-          entry8: 8<twitchSearch.data.data.length ? twitchSearch.data.data[8] : '',
-          entry9: 9<twitchSearch.data.data.length ? twitchSearch.data.data[9] : '',
-          entry10: 10<twitchSearch.data.data.length ? twitchSearch.data.data[10] : undefined,
-          entry11: 11<twitchSearch.data.data.length ? twitchSearch.data.data[11] : '',
-          entry12: 12<twitchSearch.data.data.length ? twitchSearch.data.data[12] : '',
-          entry13: 13<twitchSearch.data.data.length ? twitchSearch.data.data[13] : '',
-          entry14: 14<twitchSearch.data.data.length ? twitchSearch.data.data[14] : '',
-          entry15: 15<twitchSearch.data.data.length ? twitchSearch.data.data[15] : '',
-          entry16: 16<twitchSearch.data.data.length ? twitchSearch.data.data[16] : '',
-          entry17: 17<twitchSearch.data.data.length ? twitchSearch.data.data[17] : '',
-          entry18: 18<twitchSearch.data.data.length ? twitchSearch.data.data[18] : '',
-          entry19: 19<twitchSearch.data.data.length ? twitchSearch.data.data[19] : '',
-        }
+        search: search,
+        twitch: twitch
       });
     } else {
       throw err;
