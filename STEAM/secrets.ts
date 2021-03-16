410ce77f7d83b06a5328e6845aaf03f30431dc4c
Alessandro Brighenti <alessandro.brighenti@studenti.unitn.it>
2021-03-16 16:52:35 +0100

    ridotti i file secrets

--- STEAM/secrets.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  CLIENT_SECRET: "qdjrkcxp4q5jtqzjm1qtk46ejk20gz",
-  CLIENT_ID: "eizkab37usgvovmohkoug9x2toeg2x",
-  ITAD_ID: "997250fdd81e7711",
-  ITAD_SECRET: "174fe0520f548ee743f171a02f84ddb41fbabe7f",
-  ITAD_KEY: "ce70aa8d8dfd19342b24c7cc999c8f96d1bdda87",
-  AUTHORIZATION: "Bearer tja4hkdzhlifvxm8n6fgb8dp3c1tdj"
-}
