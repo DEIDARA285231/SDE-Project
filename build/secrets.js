54157b771685e18b8ba58f65cdf54ce3bf357896
Extrance <balasso.andrea69@gmail.com>
2021-03-17 20:55:34 +0100

    new token

--- build/secrets.js
+++ build/secrets.js
@@ -6,5 +6,5 @@ exports.default = {
     ITAD_ID: "997250fdd81e7711",
     ITAD_SECRET: "174fe0520f548ee743f171a02f84ddb41fbabe7f",
     ITAD_KEY: "ce70aa8d8dfd19342b24c7cc999c8f96d1bdda87",
-    AUTHORIZATION: "Bearer tja4hkdzhlifvxm8n6fgb8dp3c1tdj"
+    AUTHORIZATION: "Bearer uoo5y7k4xbgv53dgh764i4gejn1vmv"
 };
