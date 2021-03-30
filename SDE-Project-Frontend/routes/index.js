1aa5c9580ffe65406e0e4326b4a6d63ca6179745
Extrance <balasso.andrea69@gmail.com>
2021-03-30 20:49:49 +0200

    fixed and updated frontend

--- SDE-Project-Frontend/routes/index.js
+++ SDE-Project-Frontend/routes/index.js
@@ -93,40 +93,8 @@ router.get('/dashboard', ensureAuth, async (req,res) => {
 
     res.render('dashboard', {
       name: req.user.data.firstName,
-      twitchTop: {
-        entry0: topGames.data[0],
-        entry1: topGames.data[1],
-        entry2: topGames.data[2],
-        entry3: topGames.data[3],
-        entry4: topGames.data[4],
-        entry5: topGames.data[5],
-        entry6: topGames.data[6],
-        entry7: topGames.data[7],
-        entry8: topGames.data[8],
-        entry9: topGames.data[9],
-        entry10: topGames.data[10],
-        entry11: topGames.data[11],
-        entry12: topGames.data[12],
-        entry13: topGames.data[13],
-        entry14: topGames.data[14],
-        entry15: topGames.data[15],
-        entry16: topGames.data[16],
-        entry17: topGames.data[17],
-        entry18: topGames.data[18],
-        entry19: topGames.data[19]
-      },
-      IGDBtop: {
-        entry0: topRated.data[0],
-        entry1: topRated.data[1],
-        entry2: topRated.data[2],
-        entry3: topRated.data[3],
-        entry4: topRated.data[4],
-        entry5: topRated.data[5],
-        entry6: topRated.data[6],
-        entry7: topRated.data[7],
-        entry8: topRated.data[8],
-        entry9: topRated.data[9],
-      },
+      twitchTop: topGames.data,
+      IGDBtop: topRated.data,
       twitchStreams: {
         entry0: topStreams.data[0].user_name,
         entry1: topStreams.data[1].user_name,
@@ -232,6 +200,7 @@ router.get('/game', /*ensureAuth,*/ async (req,res) => {
       //render views/game with these params
       res.render('game', {
         data: game.data,
+        genres: game.data.genres,
         cover: url,
         prices: pricesArray
       });
